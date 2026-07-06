3917a0091765afe22f8649c440bfbedd6a939a49
schmidko <einfach.schmidt@gmail.com>
2026-07-06 08:36:32 +0200

    new logo

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,22 +1,15 @@
 import { Layout } from 'antd';
 import { Link } from 'react-router-dom';
-import logoDark from '../assets/logodark.png';
-import logoLight from '../assets/logolight.png';
-import { useThemeMode } from '../context/ThemeContext.jsx';
+import logo from '../assets/logo.png';
 import ThemeToggle from './ThemeToggle.jsx';
 
-const AppHeader = () => {
-  const { mode } = useThemeMode();
-  const logo = mode === 'dark' ? logoDark : logoLight;
-
-  return (
-    <Layout.Header className="app-header">
-      <Link to="/" className="brand">
-        <img className="brand-logo" src={logo} alt="adapools.xyz" />
-      </Link>
-      <ThemeToggle />
-    </Layout.Header>
-  );
-};
+const AppHeader = () => (
+  <Layout.Header className="app-header">
+    <Link to="/" className="brand">
+      <img className="brand-logo" src={logo} alt="adapools.xyz" />
+    </Link>
+    <ThemeToggle />
+  </Layout.Header>
+);
 
 export default AppHeader;
