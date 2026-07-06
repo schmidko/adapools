c4f37b2c1cd3d2af848f34822a8091a5f94e5fb6
schmidko <einfach.schmidt@gmail.com>
2026-07-06 08:18:55 +0200

    new logos

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,13 +1,13 @@
 import { Layout } from 'antd';
 import { Link } from 'react-router-dom';
-import logoBlack from '../assets/logoblack.png';
-import logoWhite from '../assets/logowhite.png';
+import logoDark from '../assets/logodark.png';
+import logoLight from '../assets/logolight.png';
 import { useThemeMode } from '../context/ThemeContext.jsx';
 import ThemeToggle from './ThemeToggle.jsx';
 
 const AppHeader = () => {
   const { mode } = useThemeMode();
-  const logo = mode === 'dark' ? logoBlack : logoWhite;
+  const logo = mode === 'dark' ? logoDark : logoLight;
 
   return (
     <Layout.Header className="app-header">
